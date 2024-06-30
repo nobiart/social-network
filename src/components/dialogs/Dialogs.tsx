d771d4123712e9ed790faaad05ff6b5c05d50d41
nobiart <dmitri.besleaga@gmail.com>
2024-06-30 20:12:16 +0300

    feat: L21. Dialogs Page layout

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,5 +1,30 @@
 import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
-  return <div className={s.dialogs}>Dialogs</div>
+  return (
+    <div className={s.dialogsContainer}>
+      <div className={s.dialogs}>
+        <div className={s.dialog}>
+          Ivan
+        </div>
+        <div className={`${s.dialog} ${s.activeDialog}`}>
+          Petya
+        </div>
+        <div className={s.dialog}>
+          Jora
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          Message 1
+        </div>
+        <div className={s.message}>
+          Message 2
+        </div>
+        <div className={s.message}>
+          Message 3
+        </div>
+      </div>
+    </div>
+  )
 }
