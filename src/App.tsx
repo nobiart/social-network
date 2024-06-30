ccf187403698648a2016c502605eb79c647d3881
nobiart <dmitri.besleaga@gmail.com>
2024-06-30 19:45:50 +0300

    feat: L20. React Router + NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 import {Header} from "./components/header/Header.tsx";
 import {NavBar} from "./components/navbar/NavBar.tsx";
 import {Dialogs} from "./components/dialogs/Dialogs.tsx";
-// import {Profile} from "./components/profile/Profile.tsx";
+import {Route, Routes} from "react-router-dom";
+import {Profile} from "./components/profile/Profile.tsx";
+import {News} from "./components/news/News.tsx";
+import {Music} from "./components/music/Music.tsx";
+import {Settings} from "./components/settings/Settings.tsx";
 
 // @TODO Why CSS doesn't work with "-", only camelCase
 
@@ -12,8 +16,13 @@ export const App = () => {
       <Header/>
       <NavBar/>
       <div className='app-content'>
-        <Dialogs/>
-        {/*<Profile/>*/}
+        <Routes>
+          <Route element={<Profile/>} path="/profile"/>
+          <Route element={<Dialogs/>} path="/dialogs"/>
+          <Route element={<News/>} path="/news"/>
+          <Route element={<Music/>} path="/music"/>
+          <Route element={<Settings/>} path="/settings"/>
+        </Routes>
       </div>
     </div>
   )
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,16 +1,27 @@
 import s from './NavBar.module.css';
+import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <nav className={s.navBar}>
       <ul className={s.list}>
-        <li className={s.item}><a href="#">Profile</a></li>
-        <li className={s.item}><a href="#">Messages</a></li>
-        <li className={s.item}><a href="#">News</a></li>
-        <li className={s.item}><a href="#">Music</a></li>
+        <li className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/profile">Profile</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/dialogs">Messages</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/news">News</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/music">Music</NavLink>
+        </li>
       </ul>
       <ul className={s.list}>
-        <li className={s.item}><a href="#">Settings</a></li>
+        <li className={s.item}>
+          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/settings">Settings</NavLink>
+        </li>
       </ul>
     </nav>
   )
