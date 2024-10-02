f42765a60bff82a988741844fc676697e9725178
nobiart <dmitri.besleaga@gmail.com>
2024-10-02 18:41:10 +0300

    refactor: S2-L17: Refactor Header to hooks + add Ant Design lib

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,60 @@
-import logo from "../../assets/logo.png";
-import s from './Header.module.css';
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, AppStateType} from "../../redux/reduxStore.ts";
+import {Avatar, Button, Layout, Menu, MenuProps, Space, Tooltip, Typography} from "antd";
+import {LogoutOutlined, UserOutlined} from "@ant-design/icons";
+import {AuthActionsType, logoutThunkCreator} from "../../redux/authReducer.ts";
 
-export const Header = (props: any) => {
+const topNav: MenuProps['items'] = [
+  {
+    key: "1",
+    label: <Link to="/profile">Profile</Link>,
+  },
+  {
+    key: "2",
+    label: (
+      <Link to="/users">Users</Link>
+    )
+  },
+  {
+    key: "3",
+    label: (
+      <Link to="/news">News</Link>
+    )
+  },
+];
+
+export const Header = () => {
+  const {Header} = Layout;
+  const {isAuth, login} = useSelector((state: AppStateType) => state.auth);
+
+  const dispatch: AppDispatch<AuthActionsType["type"]> = useDispatch();
+
+  const onLogout = () => {
+    dispatch(logoutThunkCreator()).then();
+  }
+
   return (
-    <header className={s.header}>
-      <img src={logo} alt=""/>
-      <div className={s.login}>
-        {props.isAuth ? (
-          <div>
-            {props.login}
-            <button onClick={props.logout}>Logout</button>
-          </div>
-        ) : <NavLink to='/login'>Login</NavLink>}
-      </div>
-    </header>
+    <Header style={{display: 'flex', alignItems: 'center'}}>
+      <div className="demo-logo"/>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        items={topNav}
+        style={{flex: 1, minWidth: 0}}
+      />
+      {isAuth ? (
+        <Space direction="horizontal">
+          <Avatar size="large" icon={<UserOutlined/>}/>
+          <Typography.Text style={{color: "white"}}>{login}</Typography.Text>
+          <Tooltip title="Logout">
+            <Button onClick={onLogout} type="default" shape="circle" icon={<LogoutOutlined/>}/>
+          </Tooltip>
+        </Space>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </Header>
   )
 };
