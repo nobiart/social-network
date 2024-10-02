cae036b00adeb2ce78c42db094dc27eb25d30b61
nobiart <dmitri.besleaga@gmail.com>
2024-10-02 18:41:58 +0300

    feat: S2-L17: Add Ant Design to navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,31 +1,57 @@
-import s from './NavBar.module.css';
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {Layout, Menu, MenuProps, theme} from "antd";
+
+const {Sider} = Layout;
+
+const sidebarNav: MenuProps['items'] = [
+  {
+    key: "sub1",
+    label: "My Profile",
+    children: [
+      {
+        key: "1",
+        label: <Link to="/profile">Profile</Link>
+      },
+      {
+        key: "2",
+        label: <Link to="/dialogs">Messages</Link>
+      },
+      {
+        key: "3",
+        label: <Link to="/settings">Settings</Link>
+      },
+    ]
+  },
+  {
+    key: "4",
+    label: <Link to="/users">Users</Link>
+  },
+  {
+    key: "5",
+    label: <Link to="/news">News</Link>
+  },
+  {
+    key: "sub2",
+    label: "Media",
+    children: [
+      {key: "6", label: <Link to="/music">Music</Link>},
+      {key: "7", label: <Link to="/video">Video</Link>},
+    ]
+  }
+];
 
 export const NavBar = () => {
+  const {token: {colorBgContainer}} = theme.useToken();
+
   return (
-    <nav className={s.navBar}>
-      <ul className={s.list}>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/profile">Profile</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/dialogs">Messages</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/users">Users</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/news">News</NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/music">Music</NavLink>
-        </li>
-      </ul>
-      <ul className={s.list}>
-        <li className={s.item}>
-          <NavLink className={({isActive}) => isActive ? s.active : ''} to="/settings">Settings</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Sider style={{background: colorBgContainer}} width={200}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{height: '100%'}}
+        items={sidebarNav}
+      />
+    </Sider>
   )
 };
